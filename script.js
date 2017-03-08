9d8096b8d69e7efb590c9fb967cb8c18f20ea044
RafM <rkjmosiolek@gmail.com>
2017-03-08 22:38:00 +0100

    jQuery fadeIn fadeOut animate functions

--- script.js
+++ script.js
@@ -5,5 +5,32 @@ $( document ).ready(function() {
         alert( "Thanks for visiting!" );
  
     });
+
+    $(".red").click(function() {
+        $(".red").animate({opacity : 0}, 600);
+
+    });
+
+    $(".blue").click(function() {
+        $(".blue").fadeOut("slow");
+    });
+
+
+    $(".orange").click(function() {
+        $(".orange").fadeOut("slow");
+    });
+
+
+    $(".green").click(function() {
+        $(".green").animate({opacity : 0}, 600);
+    });
+
+
+    $(".yellow").click(function() {
+        $(".yellow").fadeOut("slow");
+    });
+
+
+
  
 });
