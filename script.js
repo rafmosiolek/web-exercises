1177789d4b3bb9f6823054facfac7dd1606757a5
RafM <rkjmosiolek@gmail.com>
2017-03-22 10:47:34 +0100

    cash register vanilla js exercise

--- script.js
+++ script.js
@@ -1,52 +1,80 @@
-$( document ).ready(function() {
+// $( document ).ready(function() {
  
-    $( "a" ).click(function( event ) {
+//     $( "a" ).click(function( event ) {
  
-        alert( "Thanks for visiting!" );
+//         alert( "Thanks for visiting!" );
  
-    });
+//     });
 
-    $(".red").click(function() {
-        $(".red").animate({opacity : 0}, 600);
+//     $(".red").click(function() {
+//         $(".red").animate({opacity : 0}, 600);
 
-    });
+//     });
 
-    $(".blue").click(function() {
-        $(".blue").fadeOut("slow");
-    });
-
-
-    $(".orange").click(function() {
-        $(".orange").fadeOut("slow");
-    });
-
-    $(".orange").mouseover(function(){
-        $(".orange").effect("shake", {}, "fast");
-    });
+//     $(".blue").click(function() {
+//         $(".blue").fadeOut("slow");
+//     });
 
 
-    $(".green").click(function() {
-        $(".green").animate({opacity : 0}, 600);
-    });
+//     $(".orange").click(function() {
+//         $(".orange").fadeOut("slow");
+//     });
+
+//     $(".orange").mouseover(function(){
+//         $(".orange").effect("shake", {}, "fast");
+//     });
 
 
-    $(".yellow").click(function() {
-        $(".yellow").animate({down:"250px"; height:"400px"; width:"400px";});
-    });
+//     $(".green").click(function() {
+//         $(".green").animate({opacity : 0}, 600);
+//     });
 
 
-jQuery.fn.shake = function(intShakes, intDistance, intDuration) {
-    this.each(function() {
-        $(this).css("position","relative"); 
-        for (var x=1; x<=intShakes; x++) {
-        $(this).animate({left:(intDistance*-1)}, (((intDuration/intShakes)/4)))
-    .animate({left:intDistance}, ((intDuration/intShakes)/2))
-    .animate({left:0}, (((intDuration/intShakes)/4)));
-    }
-  });
-return this;
-};
+//     $(".yellow").click(function() {
+//         $(".yellow").animate({down:"250px"; height:"400px"; width:"400px";});
+//     });
+
+
+// jQuery.fn.shake = function(intShakes, intDistance, intDuration) {
+//     this.each(function() {
+//         $(this).css("position","relative"); 
+//         for (var x=1; x<=intShakes; x++) {
+//         $(this).animate({left:(intDistance*-1)}, (((intDuration/intShakes)/4)))
+//     .animate({left:intDistance}, ((intDuration/intShakes)/2))
+//     .animate({left:0}, (((intDuration/intShakes)/4)));
+//     }
+//   });
+// return this;
+// };
 
 
  
-});
+// });
+
+// var list = document.querySelector('.output ul');
+// var totalBox = document.querySelector('.output p');
+var total = 0;
+// list.innerHTML = '';
+// totalBox.textContent = '';
+// number 1
+var products = ['Underpants:6.99', 'Socks:5.99', 'T-shirt:14.99', 'Trousers:31.99', 'Shoes:23.99'];
+
+for (var i = 0; i <= products.length-1; i++) { 
+
+  var items = products[i].split(":");
+
+  var prices = Number(items[1]);
+
+ 
+    total += prices;
+    
+   itemText = items[0] + " $" + prices;
+   console.log(itemText);
+  
+  // var listItem = document.createElement('li');
+  // listItem.textContent = itemText;
+  // list.appendChild(listItem);
+}
+
+
+// totalBox.textContent = 'Total: $' + total.toFixed(2);
